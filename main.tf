24af36cfb1f6f2a99ab85eb43dd2a82b39368b10
Omarahorrar5 <omarahorar@gmail.com>
2026-02-23 13:37:10 +0000

    adding userdata to bootstrap docker

--- main.tf
+++ main.tf
@@ -70,3 +70,16 @@ resource "aws_key_pair" "test_auth" {
   key_name   = "testkey"
   public_key = file("~/.ssh/testkey.pub")
 }
+
+
+resource "aws_instance" "dev_node" {
+  ami           = data.aws_ami.server_ami.id
+  instance_type = "t3.micro"
+  key_name               = aws_key_pair.test_auth.id
+  vpc_security_group_ids = [aws_security_group.test_sg.id]
+  subnet_id              = aws_subnet.test_public_subnet.id
+
+  tags = {
+    Name = "dev-node"
+  }
+}
